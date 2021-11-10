56d303785628addccae339942025ede2f00ea933
DerTyp187 <janis.meister87@gmail.com>
2021-11-10 09:53:46 +0100

    added models

--- TealCode/main/models.py
+++ TealCode/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length= 200)
+    code_text = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category.title + " - " + self.title
--- TealCode/main/views.py
+++ TealCode/main/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Topic
 
 def index(req):
     return render(req, "main/index.html")
 
 def topic(req, category, topic):
-    return render(req, "main/topic.html")
+
+    if topic and category:
+        category_obj = Category.objects.filter(title = category).first()
+        if category_obj:
+            topic_obj = Topic.objects.filter(title=topic, category = category_obj).first()
+            if topic_obj:
+                context = {
+                    'title': topic_obj.title,
+                    'code': topic_obj.code_text,
+                    'category_title': category_obj.title,
+                }
+
+                return render(req, "main/topic.html", context)
+
+    return redirect("main-index")
+
+    
 
 def category(req, category):
-    return render(req, "main/category.html")
+    
+    if category:
+        category_obj = Category.objects.filter(title = category).first()
+        if category_obj:
+            return render(req, "main/category.html", {'category_obj': category_obj})
+        
+    
+    return redirect("main-index")
+
+
