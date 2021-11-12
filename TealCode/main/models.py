dfc4f054ed50e6b4972b69f77bd61ea02dc7fb0b
DerTyp187 <janis.meister87@gmail.com>
2021-11-12 11:01:47 +0100

    added displayname

--- TealCode/main/models.py
+++ TealCode/main/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class Category(models.Model):
     title = models.CharField(max_length=100, unique=True)
+    display_name = models.CharField(max_length= 100)
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -17,6 +18,8 @@ class Topic(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     version = models.CharField(max_length=100, blank=True)
     tags = models.CharField(max_length=200, blank=True)
+    useful = models.IntegerField(default=0)
+    not_useful = models.IntegerField(default=0)
 
     def __str__(self):
         return self.category.title + " - " + self.title
