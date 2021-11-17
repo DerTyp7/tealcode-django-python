1c11ebbfcc3089c8c5c9a32cf61beace58976836
DerTyp187 <janis.meister87@gmail.com>
2021-11-17 15:20:02 +0100

    View

--- TealCode/analytics/models.py
+++ TealCode/analytics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
+from main.models import Category,Topic
 
-# Create your models here.
+class View(models.Model):
+    ip = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    topic = models.ForeignKey(Topic, on_delete=CASCADE)
+    category = models.ForeignKey(Category, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.ip
